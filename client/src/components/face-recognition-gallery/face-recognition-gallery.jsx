b4daf89205c3a0d4c99ea46ac96c197267920f98
harshadashirsat6 <115868922+harshadashirsat6@users.noreply.github.com>
2024-03-13 13:20:12 +0530

    Update face-recognition-gallery.jsx

--- client/src/components/face-recognition-gallery/face-recognition-gallery.jsx
+++ client/src/components/face-recognition-gallery/face-recognition-gallery.jsx
@@ -45,16 +45,14 @@ const FaceRecognitionGallery = () => {
             <textarea className='fr-gallery-main-lb-textarea'
               placeholder='Edit Maximum 150 Characters for client email/Qr' />
           </div> */}
-          <div className='flex flex-col gap-2 justify-center  h-[5rem] pt-4 '>
+          <div className='flex flex-col gap-2 justify-center h-[5rem] pt-4 '>
             <h4 className='font-bold'>Share with client</h4>
             <span>PIN: 7478</span>
           </div>
-
         </div>
         <div className='fr-gallery-main-rb'>
+          <h6 className='text-center text-md'>GENERAL SETTINGS</h6>
           <div className='r1'>
-            <h6 className='pb-[2rem]'>GENERAL SETTINGS</h6>
-            {/* /////////////////// */}
             <div className='flex flex-col gap-4'>
               <div className='flex justify-between'>
                 <div className='flex justify-between'>
@@ -105,44 +103,34 @@ const FaceRecognitionGallery = () => {
                 </div>
               </div>
             </div>
-            {/* /////////////////// */}
           </div>
 
           <div className='py-6 '>
-            <h6 className='text-center'>CHOOSE TO SHARE</h6>
+            <h6 className='text-center text-md'>CHOOSE SAHRING OPTIONS</h6>
             <div className='flex justify-center gap-[10rem] py-[2rem]'>
               <div>
                 <img src={'https://th.bing.com/th?id=OIP.CKXBqkgG-DU3EG864iMU2AHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2'} className='w-[7rem] h-[7rem]' />
                 <h6 className='text-center'>Full access</h6>
+                <p className='py-2 text-xs font-bold text-center underline underline-offset-4'>COPY LINK</p>
               </div>
               <div>
                 <img src={'https://th.bing.com/th?id=OIP.CKXBqkgG-DU3EG864iMU2AHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2'} className='w-[7rem] h-[7rem]' />
                 <h6 className='text-center'>Face Seacrh</h6>
+                <p className='py-2 text-xs font-bold text-center underline underline-offset-4'>COPY LINK</p>
               </div>
             </div>
           </div>
-          {/* <div className='r3'>
-            <h6>INVITE PEOPLE</h6>
-            <div>
-              <section className='flex justify-center items-center'>
-                <div>
-                  Share with client
-                </div>
-                <div>
-                  <textarea />
-                </div>
 
-              </section>
-              <section>
-                <div>
-                  Whatsapp Number
-                </div>
-                <div>
-                  <textarea />
-                </div>
-              </section>
+          {/* share with clients */}
+          <div className='flex justify-between'>
+            <div>
+              <h5>share with clients</h5>
+              <div className=' space-x-5'>
+                <input type='email' placeholder='Client Email' className='border-[1px] border-gray-400 rounded-md px-1 py-1.5' />
+                <button className='bg-gray-200 rounded-md w-[2rem] h-[2rem] tetxt-xl font-bold text-center'>+</button>
+              </div>
             </div>
-          </div> */}
+          </div>
 
           <div id='face-gallery-form-save-btn'>
             <button>
